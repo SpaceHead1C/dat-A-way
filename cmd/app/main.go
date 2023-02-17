224f7755cce89451e363925df749f554f4ae7502
SpaceHead1C <damh17@gmail.com>
2023-02-17 23:07:02 +0300

    feat: pg repository via pgx driver

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"dataway/internal/adapter/pg"
 	"dataway/internal/migrations"
 	pkgpg "dataway/pkg/db/pg"
 	"dataway/pkg/log"
@@ -28,6 +30,22 @@ func main() {
 		panic(err.Error())
 	}
 	if err := migrations.UpMigrations(dbCC); err != nil {
+		panic(err.Error())
+	}
+	dbCtx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	repo, err := pg.NewRepository(dbCtx, pg.Config{
+		ConnectConfig: dbCC,
+		Logger:        l,
+	})
+	cancel()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer repo.Close(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := repo.Ping(ctx); err != nil {
 		panic(err.Error())
 	}
 	l.Info("dat(A)way service is up")
