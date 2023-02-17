9d01fb2851402d69e4c7aa38c7a67582f09bf5bf
SpaceHead1C <damh17@gmail.com>
2023-02-17 14:50:51 +0300

    feat: use logger in app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "os"
+import (
+	"dataway/pkg/log"
+	"os"
+)
 
 func main() {
 	c := newConfig()
 	if err := parse(os.Args[1:], c); err != nil {
 		panic(err.Error())
 	}
+	l, err := log.NewLogger()
+	if err != nil {
+		panic(err.Error())
+	}
+	l.Info("dat(A)way service is up")
 }
