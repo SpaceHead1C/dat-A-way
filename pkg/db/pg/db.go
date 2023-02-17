92a1ef1dca5790b049245837946d349779253a44
SpaceHead1C <damh17@gmail.com>
2023-02-17 22:53:55 +0300

    refactor: connection string as DSN

--- pkg/db/pg/db.go
+++ pkg/db/pg/db.go
@@ -1,11 +1,31 @@
 package pg
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
+
+type SSLMode uint
+
+const (
+	SSLDisable SSLMode = iota
+	SSLRequire
+	SSLVerifyCA
+	SSLVerifyFull
+)
+
+func (m SSLMode) String() string {
+	switch m {
+	case SSLRequire:
+		return "require"
+	case SSLVerifyCA:
+		return "verify-ca"
+	case SSLVerifyFull:
+		return "verify-full"
+	}
+	return "disable"
+}
 
 type Config struct {
 	Address      string
@@ -13,6 +33,7 @@ type Config struct {
 	User         string
 	Password     string
 	DatabaseName string
+	SSLMode      SSLMode
 }
 
 func NewConnConfig(c Config) (*pgx.ConnConfig, error) {
@@ -20,5 +41,8 @@ func NewConnConfig(c Config) (*pgx.ConnConfig, error) {
 }
 
 func connectionString(c Config) string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s", c.User, c.Password, c.Address, c.Port, c.DatabaseName)
+	return fmt.Sprintf(
+		"user=%s password=%s host=%s port=%d database=%s sslmode=%s",
+		c.User, c.Password, c.Address, c.Port, c.DatabaseName, c.SSLMode.String(),
+	)
 }
