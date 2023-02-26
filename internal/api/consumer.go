d3b8e5f47b7048ec2c848b7b55d134596eb121ae
SpaceHead1C <damh17@gmail.com>
2023-02-26 12:04:55 +0300

    feat: new consumer add method

---
+++ internal/api/consumer.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"context"
+	. "dataway/internal/domain"
+	"fmt"
+	"time"
+)
+
+const defaultConsumerManagerTimeout = time.Second
+
+type ConsumerManager struct {
+	ConsumerConfig
+}
+
+type ConsumerConfig struct {
+	Repository ConsumerRepository
+	Timeout    time.Duration
+}
+
+func NewConsumerManager(c ConsumerConfig) (*ConsumerManager, error) {
+	if c.Repository == nil {
+		return nil, fmt.Errorf("consumer repository can't be nil")
+	}
+	if c.Timeout == 0 {
+		c.Timeout = defaultConsumerManagerTimeout
+	}
+	return &ConsumerManager{c}, nil
+}
+
+func (cm *ConsumerManager) Add(ctx context.Context, req AddConsumerRequest) (*Consumer, error) {
+	ctx, cancel := context.WithTimeout(ctx, cm.Timeout)
+	defer cancel()
+	return cm.Repository.AddConsumer(ctx, req)
+}
