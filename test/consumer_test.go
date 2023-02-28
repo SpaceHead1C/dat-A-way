81be414bcc7b4c9b4f3685a379f7f2c98f331fa9
SpaceHead1C <damh17@gmail.com>
2023-02-28 15:07:49 +0300

    test: consumer update method

--- test/consumer_test.go
+++ test/consumer_test.go
@@ -5,6 +5,8 @@ import (
 	. "dataway/internal/domain"
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestAddConsumer(t *testing.T) {
@@ -12,8 +14,27 @@ func TestAddConsumer(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
 	defer cancel()
 	res, err := mngr.Add(ctx, AddConsumerRequest{
-		Name: "me",
+		Name:        "me",
 		Description: "new",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("=== Consumer ===")
+	t.Log("ID:", res.ID.String())
+	t.Log("queue:", res.Queue)
+	t.Log("name:", res.Name)
+	t.Log("description:", res.Description)
+}
+
+func TestUpdateConsumer(t *testing.T) {
+	mngr := newTestConsumerManager(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	description := "dscr"
+	res, err := mngr.Update(ctx, UpdConsumerRequest{
+		ID:          uuid.MustParse("12345678-1234-1234-1234-123456789012"),
+		Description: &description,
 	})
 	if err != nil {
 		t.Fatal(err)
