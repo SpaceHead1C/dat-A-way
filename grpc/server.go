8e781dc2eb80c3ac3f480bda2a2ac83403765d6a
SpaceHead1C <damh17@gmail.com>
2023-03-02 23:44:55 +0300

    feat: gRPC server

---
+++ grpc/server.go
@@ -0,0 +1,50 @@
+package grpc
+
+import (
+	"dataway/internal/domain"
+	"dataway/internal/pb"
+	"dataway/pkg/log"
+	"fmt"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedDatawayServer
+	logger   *zap.SugaredLogger
+	srv      *grpc.Server
+	listener net.Listener
+	port     uint
+}
+
+type Config struct {
+	Logger *zap.SugaredLogger
+	Port   uint
+}
+
+func NewServer(c Config) (domain.Server, error) {
+	var err error
+	l := c.Logger
+	if l == nil {
+		l, err = log.NewLogger()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &server{
+		logger: l,
+		port:   c.Port,
+	}, nil
+}
+
+func (s *server) Serve() error {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", s.port))
+	if err != nil {
+		return err
+	}
+	s.listener = listener
+	s.srv = grpc.NewServer()
+	pb.RegisterDatawayServer(s.srv, s)
+	return s.srv.Serve(listener)
+}
