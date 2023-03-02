125031dd7f9e7dacae612fbaff61e80755420535
SpaceHead1C <damh17@gmail.com>
2023-03-02 23:46:58 +0300

    feat: configuring and starting gRPC server

--- cmd/app/config.go
+++ cmd/app/config.go
@@ -10,6 +10,8 @@ type config struct {
 	RESTPort       uint `conf:"flag:rest_port,short:r,env:REST_PORT" toml:"rest_port" zero:"no"`
 	RESTTimeoutSec uint `conf:"flag:rest_timeout,short:r,env:REST_TIMEOUT" toml:"rest_timeout"`
 
+	GRPCPort uint `conf:"flag:grpc_port,short:r,env:GRPC_PORT" toml:"grpc_port" zero:"no"`
+
 	PostgresAddress  string `conf:"flag:postgres_address,env:POSTGRES_ADDRESS" toml:"postgres_address" zero:"no"`
 	PostgresPort     uint   `conf:"flag:postgres_port,env:POSTGRES_PORT" toml:"postgres_port" zero:"no"`
 	PostgresDBName   string `conf:"flag:postgres_db_name,env:POSTGRES_DB_NAME" toml:"postgres_db_name" zero:"no"`
