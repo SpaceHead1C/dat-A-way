ab3ac3796796b52da01d75b21f9d2ede3a8f8829
SpaceHead1C <damh17@gmail.com>
2023-03-08 00:21:32 +0300

    test: gRPC delete subscription method

--- test/grpc_test.go
+++ test/grpc_test.go
@@ -19,13 +19,36 @@ func TestPingGRPC(t *testing.T) {
 }
 
 func TestSubscribeGRPC(t *testing.T) {
-	client, conn := newGRPCClient(t)
-	defer conn.Close()
-
 	cID := uuid.MustParse("11111111-1111-1111-1111-111111111111")
 	tID := uuid.MustParse("22222222-2222-2222-2222-222222222222")
 	pID := uuid.MustParse("12345678-1234-1234-1234-123456789012")
+
+	client, conn := newGRPCClient(t)
+	defer conn.Close()
+
 	if _, err := client.Subscribe(context.Background(), &pb.Subscription{
+		ConsumerId: pb.UUIDToPb(cID),
+		TomId:      pb.UUIDToPb(tID),
+		PropertyId: pb.UUIDToPb(pID),
+	}); err != nil {
+		if s, ok := status.FromError(err); ok {
+			t.Log("error code", s.Code())
+			t.Log("error message:", s.Message())
+		} else {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestDeleteSubscriptionGRPC(t *testing.T) {
+	cID := uuid.MustParse("11111111-1111-1111-1111-111111111111")
+	tID := uuid.MustParse("22222222-2222-2222-2222-222222222222")
+	pID := uuid.MustParse("12345678-1234-1234-1234-123456789012")
+
+	client, conn := newGRPCClient(t)
+	defer conn.Close()
+
+	if _, err := client.DeleteSubscription(context.Background(), &pb.Subscription{
 		ConsumerId: pb.UUIDToPb(cID),
 		TomId:      pb.UUIDToPb(tID),
 		PropertyId: pb.UUIDToPb(pID),
