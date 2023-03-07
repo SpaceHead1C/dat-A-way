5a319e3a6436724f5150b21c584879747fed4497
SpaceHead1C <damh17@gmail.com>
2023-03-08 00:20:02 +0300

    test: gRPC subscribe method

--- test/grpc_test.go
+++ test/grpc_test.go
@@ -2,6 +2,9 @@ package test
 
 import (
 	"context"
+	"dataway/internal/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"testing"
 )
@@ -12,5 +15,26 @@ func TestPingGRPC(t *testing.T) {
 
 	if _, err := client.Ping(context.Background(), &emptypb.Empty{}); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestSubscribeGRPC(t *testing.T) {
+	client, conn := newGRPCClient(t)
+	defer conn.Close()
+
+	cID := uuid.MustParse("11111111-1111-1111-1111-111111111111")
+	tID := uuid.MustParse("22222222-2222-2222-2222-222222222222")
+	pID := uuid.MustParse("12345678-1234-1234-1234-123456789012")
+	if _, err := client.Subscribe(context.Background(), &pb.Subscription{
+		ConsumerId: pb.UUIDToPb(cID),
+		TomId:      pb.UUIDToPb(tID),
+		PropertyId: pb.UUIDToPb(pID),
+	}); err != nil {
+		if s, ok := status.FromError(err); ok {
+			t.Log("error code", s.Code())
+			t.Log("error message:", s.Message())
+		} else {
+			t.Fatal(err)
+		}
 	}
 }
