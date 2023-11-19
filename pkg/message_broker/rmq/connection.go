24f08dfc61f53dbbb9323a9abd131decacfeda42
SpaceHead1C <damh17@gmail.com>
2023-11-19 23:02:46 +0300

    feat: rmq connection with reconnect

--- pkg/message_broker/rmq/connection.go
+++ pkg/message_broker/rmq/connection.go
@@ -4,67 +4,117 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync"
+	"time"
 
 	"dataway/pkg/log"
 
-	rmq "github.com/wagslane/go-rabbitmq"
-	"go.uber.org/zap"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const (
-	defaultUserRMQ     = "guest"
-	defaultPasswordRMQ = "guest"
+	defaultPort uint16 = 5672
+
+	defaultUser     = "guest"
+	defaultPassword = "guest"
 )
 
 type ConnectionConfig struct {
-	Address  string
-	Port     uint
 	Logger   *log.Logger
+	Host     string
+	Port     uint16
 	User     string
 	Password string
 	VHost    string
 }
 
 type Connection struct {
-	conn *rmq.Conn
+	url  string
+	conn *amqp.Connection
+	ch   *channel
+	mu   *sync.RWMutex
 	l    *log.Logger
 }
 
-func NewConnection(c ConnectionConfig) (*Connection, error) {
-	if strings.TrimSpace(c.Address) == "" {
-		return nil, errors.New("RMQ address can not be empty")
+func New(c ConnectionConfig) (*Connection, error) {
+	if c.Host == "" {
+		return nil, errors.New("address can not be empty")
 	}
 	if c.Port == 0 {
-		c.Port = 5672
+		c.Port = defaultPort
 	}
 	if c.Logger == nil {
 		c.Logger = log.GlobalLogger()
 	}
 	if strings.TrimSpace(c.User) == "" {
-		c.User = defaultUserRMQ
+		c.User = defaultUser
 	}
 	if strings.TrimSpace(c.Password) == "" {
-		c.Password = defaultPasswordRMQ
+		c.Password = defaultPassword
 	}
-	conn, err := rmq.NewConn(
-		connectionString(c),
-		rmq.WithConnectionOptionsLogger(logger{c.Logger}),
-	)
+	url := connectionString(c)
+	conn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	return &Connection{
+	out := &Connection{
+		url:  url,
 		conn: conn,
+		mu:   &sync.RWMutex{},
 		l:    c.Logger,
-	}, nil
+	}
+	out.ch, err = out.channel()
+	if err != nil {
+		return nil, err
+	}
+	go out.startNotifyClose()
+	return out, nil
 }
 
 func (conn *Connection) Close() {
+	conn.mu.Lock()
+	defer conn.mu.Unlock()
 	if err := conn.conn.Close(); err != nil {
-		conn.l.Errorf("rmq connection close error: %s", err)
+		conn.l.Errorf("connection close error: %s", err)
 	}
 }
 
 func connectionString(c ConnectionConfig) string {
-	return fmt.Sprintf("amqp://%s:%s@%s:%d/%s", c.User, c.Password, c.Address, c.Port, c.VHost)
+	return fmt.Sprintf("amqp://%s:%s@%s:%d/%s", c.User, c.Password, c.Host, c.Port, c.VHost)
+}
+
+func (conn *Connection) startNotifyClose() {
+	notifyCloseChan := conn.conn.NotifyClose(make(chan *amqp.Error, 1))
+	err := <-notifyCloseChan
+	if err != nil {
+		conn.l.Errorf("connection close with error: %s\nReconnecting...", err)
+		conn.reconnecting()
+		conn.l.Info("successfully reconnected to amqp server")
+	}
+}
+
+func (conn *Connection) reconnecting() {
+	for {
+		time.Sleep(time.Second)
+		err := conn.reconnect()
+		if err == nil {
+			go conn.startNotifyClose()
+			return
+		}
+		conn.l.Error(err.Error())
+	}
+}
+
+func (conn *Connection) reconnect() error {
+	conn.mu.Lock()
+	defer conn.mu.Unlock()
+	newConn, err := amqp.Dial(conn.url)
+	if err != nil {
+		return err
+	}
+	if err = conn.conn.Close(); err != nil {
+		conn.l.Errorf("close channel error: %s", err)
+	}
+	conn.conn = newConn
+	return nil
 }
